061876ec4bcd0f06ba2168412f8cff51710f6edb
Emre Yildirim <temreyildirim8@gmail.com>
2024-08-09 23:27:45 +0300

    revert: back to last commit 9f82849a19e2eeef23506f430a066536fd9e87d6 because we needed to reset the test, sorry

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { useAtom } from 'jotai';
 import { RootStackParamList } from '../types/navigation';
 import useColors from '../hooks/useColors';
 import BackButtonSvg from './BackButtonSvg';
+import {
+  questionsAtom,
+  correctAnswersAtom,
+  currentQuestionIndexAtom,
+} from '../atoms/questionsAtom';
 
 type HeaderProps = {
   title: string;
@@ -14,12 +20,22 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ title, showBackButton = false }) => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const colors = useColors();
+  const [, setQuestions] = useAtom(questionsAtom);
+  const [, setCorrectAnswers] = useAtom(correctAnswersAtom);
+  const [, setCurrentQuestionIndex] = useAtom(currentQuestionIndexAtom);
+
+  const handleGoBack = () => {
+    setQuestions([]); // We are doing this to reset the this entirely
+    setCorrectAnswers(0);
+    setCurrentQuestionIndex(0);
+    navigation.goBack();
+  };
 
   return (
     <View style={[styles.header, { backgroundColor: colors.bgGeneric }]}>
       {showBackButton && (
         <TouchableOpacity
-          onPress={() => navigation.goBack()}
+          onPress={() => handleGoBack()}
           style={styles.backButton}
           testID="back-button"
         >
