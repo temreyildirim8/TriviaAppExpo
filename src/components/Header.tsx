80a68c8aba0ac8e2de95eec2c75af09cc9541237
Emre Yildirim <temreyildirim8@gmail.com>
2024-08-09 22:39:17 +0300

    fix: reset the test on back button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { useAtom } from 'jotai';
 import { RootStackParamList } from '../types/navigation';
 import useColors from '../hooks/useColors';
 import BackButtonSvg from './BackButtonSvg';
+import {
+  questionsAtom,
+  correctAnswersAtom,
+  currentQuestionIndexAtom,
+} from '../atoms/questionsAtom';
 
 type HeaderProps = {
   title: string;
@@ -14,11 +20,25 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ title, showBackButton = false }) => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const colors = useColors();
+  const [, setQuestions] = useAtom(questionsAtom);
+  const [, setCorrectAnswers] = useAtom(correctAnswersAtom);
+  const [, setCurrentQuestionIndex] = useAtom(currentQuestionIndexAtom);
+
+  const handleGoBack = () => {
+    setQuestions([]); // We are doing this to reset the this entirely
+    setCorrectAnswers(0);
+    setCurrentQuestionIndex(0);
+    navigation.goBack();
+  };
 
   return (
     <View style={[styles.header, { backgroundColor: colors.bgGeneric }]}>
       {showBackButton && (
-        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton} testID="back-button">
+        <TouchableOpacity
+          onPress={() => handleGoBack()}
+          style={styles.backButton}
+          testID="back-button"
+        >
           <BackButtonSvg width={24} height={24} color={colors.white} />
         </TouchableOpacity>
       )}
@@ -33,12 +53,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'flex-end',
     justifyContent: 'center',
-    paddingBottom: 20
+    paddingBottom: 20,
   },
   backButton: {
     position: 'absolute',
     left: 20,
-    paddingBottom: 20
+    paddingBottom: 20,
   },
   title: {
     fontSize: 20,
