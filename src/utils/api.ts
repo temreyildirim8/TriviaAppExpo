89399eb272eb28da30076a5e473a61f78618d19a
Emre Yildirim <temreyildirim8@gmail.com>
2024-08-09 16:04:18 +0300

    fix: add catch for axios request

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,11 @@
-import axios from 'axios';
-import { Question } from '../atoms/questionsAtom';
+import axios from "axios";
+import { Question } from "../atoms/questionsAtom";
 
 export const fetchQuestions = async (): Promise<Question[]> => {
-  const response = await axios.get('https://opentdb.com/api.php?amount=4&type=boolean');
-  return response.data.results.map((question: any) => ({
+  const response = await axios
+    .get("https://opentdb.com/api.php?amount=4&type=boolean")
+    .catch((e) => console.error(e));
+  return response?.data.results.map((question: any) => ({
     question: question.question,
     correct_answer: question.correct_answer,
   }));
